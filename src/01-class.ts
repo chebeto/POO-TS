4612a162cbdfd9ef4b72a6cb9cea3029812b678d
Oswaldo Sánchez <oswaldo.sanchez@me.com>
2022-04-15 22:23:21 -0500

    FirstSession

---
+++ src/01-class.ts
@@ -0,0 +1,16 @@
+export class MyDate
+{
+    year: number;
+    month: number;
+    day: number;
+
+    constructor(year: number, month: number, day: number)
+    {
+        this.year   = year;
+        this.month  = month;
+        this.day    = day;
+    }
+}
+
+const myDate = new MyDate(2021, 3, 17);
+console.log(myDate);
